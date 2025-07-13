502d877d87ed4315c47a784703410b494eb92bcf
Bernardo Marques <bernardohmarques@gmail.com>
2025-07-13 15:39:55 -0300

    feat: add typescript and linting

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import { createServer } from "./server.js";
+
+config();
+
+const init = async () => {
+  try {
+    const app = createServer();
+
+    const port = process.env.PORT || 1000;
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to initialize application:", err);
+  }
+};
+
+init();
